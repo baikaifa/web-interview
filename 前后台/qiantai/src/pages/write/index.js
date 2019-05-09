65c5a288062fb2cf98790af7558084774ce24906
baikaifa <1956841815@qq.com>
2019-05-09 09:46:03 +0800

    需要merge master

--- 前后台/qiantai/src/pages/write/index.js
+++ 前后台/qiantai/src/pages/write/index.js
@@ -1,23 +1,35 @@
 import React, { PureComponent } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { HomeWrapper, HomeRight, LeftWhite, RightWhite } from '../home/style.js'
+import  {HomeLeft,NewArticle,NoTitleArticle} from './style.js'
 class Write extends PureComponent {
     render() {
         const { loginStatus } = this.props;
         console.log(loginStatus);
         if (loginStatus) {
             return (
-            <div>写文章</div>
+                <HomeWrapper>
+                    <HomeLeft>
+                        <NewArticle>新建文章</NewArticle>
+                        <NoTitleArticle>无标题文章</NoTitleArticle>
+                    </HomeLeft>
+                    <HomeRight>
+                        456
+                    </HomeRight>
+                    <LeftWhite></LeftWhite>
+                    <RightWhite></RightWhite>
+                </HomeWrapper>
             )
         } else {
             return <Redirect to='/login' />
-                }
-            }
         }
-        
+    }
+}
+
 const mapState = (state) => ({
-                    loginStatus: state.getIn(['login', 'login'])
-            })
-            
+    loginStatus: state.getIn(['login', 'login'])
+})
+
 
 export default connect(mapState, null)(Write);
